91db23df5236392e1e03ba479f01c75dfc67699c
Dlamim <dionisiolm@gmail.com>
2023-04-26 09:39:42 -0300

    adding files

---
+++ vr-va-beneficios/src/main/java/com/vrvabeneficios/vrvabeneficios/VrVaBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package com.vrvabeneficios.vrvabeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrVaBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrVaBeneficiosApplication.class, args);
+	}
+
+}
